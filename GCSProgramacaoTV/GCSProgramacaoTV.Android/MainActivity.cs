b9a6d38b6c11eeff03685d0900690b363ad8f0e6
Welisson <welisson@nucleologuin.com.br>
2018-03-17 18:39:26 -0300

    Colocar tela de Splash #1

--- GCSProgramacaoTV/GCSProgramacaoTV.Android/MainActivity.cs
+++ GCSProgramacaoTV/GCSProgramacaoTV.Android/MainActivity.cs
@@ -6,7 +6,9 @@ using Prism.Ioc;
 
 namespace GCSProgramacaoTV.Droid
 {
-    [Activity(Label = "GCSProgramacaoTV", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "GCSProgramacaoTV", Icon = "@drawable/icon", 
+        Theme = "@style/MainTheme", MainLauncher = false, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
